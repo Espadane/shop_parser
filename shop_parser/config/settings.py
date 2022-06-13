ac9a11a92a7d646f968d7772e4bd61f6ba8e094d
Espadane <espadane@gmail.com>
2022-06-13 13:31:49 +0300

    add secret

--- shop_parser/config/settings.py
+++ shop_parser/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$m(u11!7owe$9g_n!n9_r(v!5%(v+_@ro9g471wgwcr!i*or0f'
+SECRET_KEY = os.getenv('SHOP_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
